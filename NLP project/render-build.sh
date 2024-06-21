5cd51044c3e77c40cc9a155edd29ac0841fd16ed
Grapefruitss <162535081+Grapefruitss@users.noreply.github.com>
2024-06-22 00:27:19 +0900

    Update render-build.sh

--- NLP project/render-build.sh
+++ NLP project/render-build.sh
@@ -8,9 +8,9 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo dpkg -i google-chrome-stable_current_amd64.deb || sudo apt-get install -f -y
+  rm google-chrome-stable_current_amd64.deb
   cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
