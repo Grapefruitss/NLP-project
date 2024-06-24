514b8607f7608c7c187e2bb9c67ffebe8d938e1a
Grapefruitss <162535081+Grapefruitss@users.noreply.github.com>
2024-06-24 14:38:00 +0900

    Update render-build.sh

--- NLP project/render-build.sh
+++ NLP project/render-build.sh
@@ -2,45 +2,16 @@
 # exit on error
 set -o errexit
 
-start_time=$(date +%s)
-
-echo "Starting directory: $(pwd)"
-
 STORAGE_DIR=/opt/render/project/.render
-TEMP_DIR=/tmp/apt
 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  
-  # Set the TMPDIR environment variable
-  export TMPDIR=$TEMP_DIR
-  
-  # Create the necessary directories with write permissions
-  mkdir -p $TEMP_DIR/apt/lists/partial
-  chmod -R 777 $TEMP_DIR/apt
-  
-  # Create the status file manually
-  touch $TEMP_DIR/apt/status
-  chmod 666 $TEMP_DIR/apt/status
-  
-  apt-get update -o Dir::State::lists="$TEMP_DIR/apt/lists" -o Dir::State::status="$TEMP_DIR/apt/status"
-  apt-get install -y --no-install-recommends libgtk2.0-dev libgtk-3-dev libgbm-dev libx11-xcb-dev libxcomposite-dev \
-                     libxcursor-dev libxdamage-dev libxi-dev libxrandr-dev libxss-dev libxtst-dev \
-                     libatk1.0-dev libatk-bridge2.0-dev libpangocairo-1.0-0 libcups2-dev libfontconfig1-dev \
-                     libdbus-1-dev libexpat1-dev
-  dpkg -i google-chrome-stable_current_amd64.deb
-  rm google-chrome-stable_current_amd64.deb
-  # Check if the directory exists before changing to it
-  if [[ -d "$HOME/project/src" ]]; then
-    cd $HOME/project/src
-    echo "Changed directory to $HOME/project/src"
-  else
-    echo "Directory $HOME/project/src does not exist. Please check the path."
-    exit 1
-  fi
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
 fi
