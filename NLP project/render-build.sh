dd2d3c68405ddbe1c7d3e7126e8f906639c8fb70
Grapefruitss <162535081+Grapefruitss@users.noreply.github.com>
2024-06-24 14:50:23 +0900

    Update render-build.sh

--- NLP project/render-build.sh
+++ NLP project/render-build.sh
@@ -11,7 +11,7 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Make sure we return to where we were
+  cd /opt/render/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
 fi
@@ -20,9 +20,9 @@ fi
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
 # Check if requirements.txt exists in the expected directory
-if [[ -f $HOME/project/src/requirements.txt ]]; then
+if [[ -f /opt/render/project/requirements.txt ]]; then
   echo "Installing Python dependencies..."
-  pip install -r $HOME/project/src/requirements.txt --no-cache-dir -q
+  pip install -r /opt/render/project/requirements.txt --no-cache-dir -q
 else
   echo "ERROR: Could not find requirements.txt in the expected directory."
   exit 1
