73718f6f49277da6d8996d155aa943078354b69a
Grapefruitss <162535081+Grapefruitss@users.noreply.github.com>
2024-06-24 14:56:08 +0900

    Update render-build.sh

--- NLP project/render-build.sh
+++ NLP project/render-build.sh
@@ -17,7 +17,7 @@ else
 fi
 
 # be sure to add Chrome's location to the PATH as part of your Start Command
-export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+#export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt --no-cache-dir -q
